e6974fedf072f881dbfcaf02b0fe5e9ede1c31e2
anshumannjs <anshumannarendrajitsingh@gmail.com>
2023-08-30 17:35:37 +0530

    made Fund Me

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.18;
+pragma solidity ^0.8.21;
 
-import {Test} from "forge-std/Test.sol"
+import {Test} from "forge-std/Test.sol";
 
 contract FundMeTest is Test{
     function setUp() external{
